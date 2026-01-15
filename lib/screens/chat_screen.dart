28b59fcbd33852aa1260565afb64a615145371bb
Himanshu Yadav <Hiimanshuyadav11@gmail.com>
2026-01-15 22:58:56 +0530

    Chat input and auto reply working

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -10,21 +10,51 @@ class ChatScreen extends StatefulWidget {
 class _ChatScreenState extends State<ChatScreen> {
   final List<Map<String,dynamic>>messages = [
     {'text':"Hi , Healsync here",
-    'isuser':false}
-  ];
+    'isuser':false,},];
+  final TextEditingController _controller = TextEditingController();
+  
   @override
   Widget build(BuildContext context) {
     return  Scaffold(
-      body: ListView.builder(
+      body: Column(
+        children: [
+          Expanded(child: ListView.builder(
         padding: EdgeInsets.all(12),
         itemCount: messages.length ,
         itemBuilder: (context, index){
-          return Container( 
+          return Align(alignment: messages[index]['isuser']
+          ?Alignment.centerRight
+          :Alignment.centerLeft,
+          child :Container( 
             padding: EdgeInsets.all(12),
             margin: EdgeInsets.symmetric(vertical: 6),
             decoration: BoxDecoration(color: Colors.grey, borderRadius: BorderRadius.circular(16)),
-            child: Text(messages[index]['text'],style: TextStyle(color: Colors.white)),);
+            child: Text(messages[index]['text'],style: TextStyle(color: Colors.white)),));
         }
       )
+       ),
+       Container(color: Colors.blue,
+       padding: EdgeInsets.symmetric(horizontal: 8, vertical: 6),
+       child: Row(
+        children: [Expanded(child: TextField(
+          controller: _controller,
+          style: TextStyle(color: Colors.white),
+          decoration: InputDecoration(hintText: 'U All Right ?',
+          hintStyle: TextStyle(color: Colors.amber),border: InputBorder.none),
+        )),IconButton(onPressed: (){
+          if(_controller.text.trim().isEmpty)return;
+          setState(() {
+            messages.add({'text': _controller.text.trim(),
+            'isuser':true,});
+          });_controller.clear();
+        }, icon: Icon(Icons.send,color: Colors.blueAccent,))],
+       ),
+       )
+       ],
+       
+      ),
+      
+      
+      
       );
   }}
